f6f7170f7403e57c50ff21afd887c04399345699
kimiyash <kimiyash@gmail.com>
2024-09-07 15:31:59 +0900

    tweek samplecli

--- chapter_4/samplecli/src/main.rs
+++ chapter_4/samplecli/src/main.rs
@@ -2,6 +2,51 @@ use clap::{App, Arg};
 use std::fs::File;
 use std::io::{stdin, BufRead, BufReader};
 
+struct RpnCalcurator(bool);
+
+impl RpnCalcurator {
+    pub fn new(verbose: bool) -> Self {
+        Self(verbose)
+    }
+
+    pub fn eval(&self, formula: &str) -> i32 {
+        let mut tokens = formula.split_whitespace().rev().collect::<Vec<_>>();
+        self.eval_inner(&mut tokens)
+    }
+
+    fn eval_inner(&self, tokens: &mut Vec<&str>) -> i32 {
+        let mut stack = Vec::new();
+
+        while let Some(token) = tokens.pop() {
+            if let Ok(x) = token.parse::<i32>() {
+                stack.push(x);
+            } else {
+                let y = stack.pop().expect("invalid syntax");
+                let x = stack.pop().expect("invalid syntax");
+
+                let res = match token {
+                    "+" => x + y,
+                    "-" => x - y,
+                    "*" => x * y,
+                    "/" => x / y,
+                    "%" => x % y,
+                    _ => panic!("invalid token"),
+                };
+                stack.push(res);
+            }
+
+            if self.0 {
+                println!("{:?} {:?}", tokens, stack);
+            }
+        }
+        if stack.len() == 1 {
+            stack[0]
+        } else {
+            panic!("invalid syntax")
+        }
+    }
+}
+
 fn main() {
     let matches = App::new("My RPN program")
         .version("1.0.0")
@@ -22,7 +67,6 @@ fn main() {
         .get_matches();
 
     let verbose = matches.is_present("verbose");
-    println!("Is verbosity specified?: {}", verbose);
 
     if let Some(path) = matches.value_of("formula_file") {
         let f = File::open(path).unwrap();
@@ -37,8 +81,10 @@ fn main() {
 }
 
 fn run<R: BufRead>(reader: R, verbose: bool) {
+    let calc = RpnCalcurator::new(verbose);
     for line in reader.lines() {
         let line = line.unwrap();
-        println!("{}", line);
+        let answer = calc.eval(&line);
+        println!("{}", answer);
     }
 }
