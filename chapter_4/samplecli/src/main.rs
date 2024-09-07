3d840e89428b268909f7942f09994ad1812ff14a
kimiyash <kimiyash@gmail.com>
2024-09-07 15:10:28 +0900

    tweek samplecli

--- chapter_4/samplecli/src/main.rs
+++ chapter_4/samplecli/src/main.rs
@@ -1,4 +1,6 @@
 use clap::{App, Arg};
+use std::fs::File;
+use std::io::{stdin, BufRead, BufReader};
 
 fn main() {
     let matches = App::new("My RPN program")
@@ -19,10 +21,24 @@ fn main() {
         )
         .get_matches();
 
-    match matches.value_of("formula_file") {
-        Some(file) => println!("File specified: {}", file),
-        None => println!("No file specified"),
-    }
     let verbose = matches.is_present("verbose");
     println!("Is verbosity specified?: {}", verbose);
+
+    if let Some(path) = matches.value_of("formula_file") {
+        let f = File::open(path).unwrap();
+        let reader = BufReader::new(f);
+        run(reader, verbose);
+    } else {
+        let stdin = stdin();
+        let reader = stdin.lock();
+        run(reader, verbose);
+    }
+
+}
+
+fn run<R: BufRead>(reader: R, verbose: bool) {
+    for line in reader.lines() {
+        let line = line.unwrap();
+        println!("{}", line);
+    }
 }
